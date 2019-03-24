5972313a210f172b624a0212ecc8cf6007189258
Dave Collier <lardelbow@gmail.com>
2019-03-24 21:25:43 +0000

    IncidentComponent spec testes completed.

--- src/test/javascript/spec/app/entities/incident/incident.component.spec.ts
+++ src/test/javascript/spec/app/entities/incident/incident.component.spec.ts
@@ -1,19 +1,22 @@
 /* tslint:disable max-line-length */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
 import { ActivatedRoute, Data } from '@angular/router';
-
 import { ApplicationSupportDashboardTestModule } from '../../../test.module';
 import { IncidentComponent } from 'app/entities/incident/incident.component';
 import { IncidentService } from 'app/entities/incident/incident.service';
 import { Incident } from 'app/shared/model/incident.model';
+import { IncidentUpdatesService } from 'app/entities/incident-updates';
+import { ProblemUpdatesService } from 'app/entities/problem-updates';
 
 describe('Component Tests', () => {
     describe('Incident Management Component', () => {
         let comp: IncidentComponent;
         let fixture: ComponentFixture<IncidentComponent>;
         let service: IncidentService;
+        let service1: IncidentUpdatesService;
+        let service2: ProblemUpdatesService;
 
         beforeEach(() => {
             TestBed.configureTestingModule({
@@ -43,6 +46,8 @@ describe('Component Tests', () => {
             fixture = TestBed.createComponent(IncidentComponent);
             comp = fixture.componentInstance;
             service = fixture.debugElement.injector.get(IncidentService);
+            service1 = fixture.debugElement.injector.get(IncidentUpdatesService);
+            service2 = fixture.debugElement.injector.get(ProblemUpdatesService);
         });
 
         it('Should call load all on init', () => {
@@ -63,6 +68,29 @@ describe('Component Tests', () => {
             // THEN
             expect(service.query).toHaveBeenCalled();
             expect(comp.incidents[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+        });
+
+        it('should clear the filter values', () => {
+            // GIVEN
+            comp.selectedSeverity = 'P1';
+            comp.selectedStatus = 'OPEN';
+
+            // WHEN
+            comp.onClearFilter();
+
+            // THEN
+            expect(comp.selectedStatus).toEqual('ALL');
+            expect(comp.selectedSeverity).toEqual('ALL');
+        });
+
+        it('should give return routes', () => {
+            // WHEN
+            comp.setIncidentUpdatesReturnPage(1);
+            comp.setProblemUpdatesReturnPage(1);
+
+            // THEN
+            expect(service1.returnRoute).toEqual('/incident');
+            expect(service2.returnRoute).toEqual('/incident');
         });
 
         it('should load a page', () => {
@@ -106,6 +134,7 @@ describe('Component Tests', () => {
             expect(service.query).toHaveBeenCalledTimes(2);
             expect(comp.incidents[0]).toEqual(jasmine.objectContaining({ id: 123 }));
         });
+
         it('should calculate the sort attribute for an id', () => {
             // WHEN
             const result = comp.sort();
