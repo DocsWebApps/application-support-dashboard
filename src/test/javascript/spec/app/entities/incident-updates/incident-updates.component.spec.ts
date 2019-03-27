d361fdbb9c83e9177689488e1c5793d459895f37
Dave Collier <lardelbow@gmail.com>
2019-03-27 17:56:18 +0000

    Beefed up incident-updates tests.

--- src/test/javascript/spec/app/entities/incident-updates/incident-updates.component.spec.ts
+++ src/test/javascript/spec/app/entities/incident-updates/incident-updates.component.spec.ts
@@ -1,9 +1,8 @@
 /* tslint:disable max-line-length */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
-import { ActivatedRoute, Data } from '@angular/router';
-
+import { ActivatedRoute, Data, Route } from '@angular/router';
 import { ApplicationSupportDashboardTestModule } from '../../../test.module';
 import { IncidentUpdatesComponent } from 'app/entities/incident-updates/incident-updates.component';
 import { IncidentUpdatesService } from 'app/entities/incident-updates/incident-updates.service';
@@ -23,6 +22,9 @@ describe('Component Tests', () => {
                     {
                         provide: ActivatedRoute,
                         useValue: {
+                            snapshot: {
+                                params: { incidentID: 123 }
+                            },
                             data: {
                                 subscribe: (fn: (value: Data) => void) =>
                                     fn({
@@ -45,25 +47,25 @@ describe('Component Tests', () => {
             service = fixture.debugElement.injector.get(IncidentUpdatesService);
         });
 
-        // it('Should call load all on init', () => {
-        //     // GIVEN
-        //     const headers = new HttpHeaders().append('link', 'link;link');
-        //     spyOn(service, 'query').and.returnValue(
-        //         of(
-        //             new HttpResponse({
-        //                 body: [new IncidentUpdates(123)],
-        //                 headers
-        //             })
-        //         )
-        //     );
-        //
-        //     // WHEN
-        //     comp.ngOnInit();
-        //
-        //     // THEN
-        //     expect(service.query).toHaveBeenCalled();
-        //     expect(comp.incidentUpdates[0]).toEqual(jasmine.objectContaining({ id: 123 }));
-        // });
+        it('Should call load all on init', () => {
+            // GIVEN
+            const headers = new HttpHeaders().append('link', 'link;link');
+            spyOn(service, 'query').and.returnValue(
+                of(
+                    new HttpResponse({
+                        body: [new IncidentUpdates(123)],
+                        headers
+                    })
+                )
+            );
+
+            // WHEN
+            comp.ngOnInit();
+
+            // THEN
+            expect(service.query).toHaveBeenCalled();
+            expect(comp.incidentUpdates[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+        });
 
         it('should load a page', () => {
             // GIVEN
@@ -84,46 +86,46 @@ describe('Component Tests', () => {
             expect(service.query).toHaveBeenCalled();
             expect(comp.incidentUpdates[0]).toEqual(jasmine.objectContaining({ id: 123 }));
         });
-        //
-        // it('should re-initialize the page', () => {
-        //     // GIVEN
-        //     const headers = new HttpHeaders().append('link', 'link;link');
-        //     spyOn(service, 'query').and.returnValue(
-        //         of(
-        //             new HttpResponse({
-        //                 body: [new IncidentUpdates(123)],
-        //                 headers
-        //             })
-        //         )
-        //     );
-        //
-        //     // WHEN
-        //     comp.loadPage(1);
-        //     comp.reset();
-        //
-        //     // THEN
-        //     expect(comp.page).toEqual(0);
-        //     expect(service.query).toHaveBeenCalledTimes(2);
-        //     expect(comp.incidentUpdates[0]).toEqual(jasmine.objectContaining({ id: 123 }));
-        // });
-        //
-        // it('should calculate the sort attribute for an id', () => {
-        //     // WHEN
-        //     const result = comp.sort();
-        //
-        //     // THEN
-        //     expect(result).toEqual(['id,asc']);
-        // });
-        //
-        // it('should calculate the sort attribute for a non-id attribute', () => {
-        //     // GIVEN
-        //     comp.predicate = 'name';
-        //
-        //     // WHEN
-        //     const result = comp.sort();
-        //
-        //     // THEN
-        //     expect(result).toEqual(['name,asc', 'id']);
-        // });
+
+        it('should re-initialize the page', () => {
+            // GIVEN
+            const headers = new HttpHeaders().append('link', 'link;link');
+            spyOn(service, 'query').and.returnValue(
+                of(
+                    new HttpResponse({
+                        body: [new IncidentUpdates(123)],
+                        headers
+                    })
+                )
+            );
+
+            // WHEN
+            comp.loadPage(1);
+            comp.reset();
+
+            // THEN
+            expect(comp.page).toEqual(0);
+            expect(service.query).toHaveBeenCalledTimes(2);
+            expect(comp.incidentUpdates[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+        });
+
+        it('should calculate the sort attribute for an id', () => {
+            // WHEN
+            const result = comp.sort();
+
+            // THEN
+            expect(result).toEqual(['id,asc']);
+        });
+
+        it('should calculate the sort attribute for a non-id attribute', () => {
+            // GIVEN
+            comp.predicate = 'name';
+
+            // WHEN
+            const result = comp.sort();
+
+            // THEN
+            expect(result).toEqual(['name,asc', 'id']);
+        });
     });
 });
